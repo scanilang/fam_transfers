3fd69ba5077ddad3514b7c0580c9e4b62c19c3a0
Sara Canilang <canil007@ahl03.agate.msi.umn.edu>
2026-04-30 16:32:58 -0500

    solve

--- fam_transfers_solve.jl
+++ fam_transfers_solve.jl
@@ -6,60 +6,68 @@ function solve_model(model, savepath)
     # -----------------------------------------------------------------------
     # NO COLLEGE PATH
     # -----------------------------------------------------------------------
+    if !isfile(joinpath(savepath, "NoSchool.jls")) 
+        
+        # --- Retirement W_nc ---
+        println("Solving No College Retirement...")
+        # Dims: (j, R, m, n, t, i_a, i_z, shock_in, shock_out, past_in, past_out) = 10 non-j
+        Wj_nc  = zeros(Float32, 2, 2, 5, 3, apnts_nc, zpnts, 2, 2, 2, 2)
+        WPFj_nc = copy(Wj_nc)
 
-    # --- Retirement W_nc ---
-    println("Solving No College Retirement...")
-    # Dims: (j, R, m, n, t, i_a, i_z, shock_in, shock_out, past_in, past_out) = 10 non-j
-    Wj_nc  = zeros(Float32, 2, 2, 5, 3, apnts_nc, zpnts, 2, 2, 2, 2)
-    WPFj_nc = copy(Wj_nc)
+        W_nc  = zeros(Float32, n_retirement, 2, 2, 5, 3, apnts_nc, zpnts, 2, 2, 2, 2)
+        WPF_nc = copy(W_nc)
 
-    W_nc  = zeros(Float32, n_retirement, 2, 2, 5, 3, apnts_nc, zpnts, 2, 2, 2, 2)
-    WPF_nc = copy(W_nc)
+        W_nc[n_retirement,  :,:,:,:,:,:,:,:,:,:], WPF_nc[n_retirement, :,:,:,:,:,:,:,:,:,:] = Wncj(nothing, Wj_nc, WPFj_nc, model, jpnts)
 
-    W_nc[n_retirement,  :,:,:,:,:,:,:,:,:,:], WPF_nc[n_retirement, :,:,:,:,:,:,:,:,:,:] = Wncj(nothing, Wj_nc, WPFj_nc, model, jpnts)
+        for j in (jpnts-1):-1:(working_years+1)
+            idx = j - working_years
+            W_jp1 = @view W_nc[idx+1, :,:,:,:,:,:,:,:,:,:]
+            W_nc[idx,  :,:,:,:,:,:,:,:,:,:], 
+            WPF_nc[idx, :,:,:,:,:,:,:,:,:,:] = Wncj(W_jp1, Wj_nc, WPFj_nc, model, j)
+        end
 
-    for j in (jpnts-1):-1:(working_years+1)
-        idx = j - working_years
-        W_jp1 = @view W_nc[idx+1, :,:,:,:,:,:,:,:,:,:]
-        W_nc[idx,  :,:,:,:,:,:,:,:,:,:], 
-        WPF_nc[idx, :,:,:,:,:,:,:,:,:,:] = Wncj(W_jp1, Wj_nc, WPFj_nc, model, j)
-    end
+        # --- Post-family-shock working Vnc2 ---
+        println("Solving No College Post-Family-Shock Working...")
+        # Dims: (j, R, m, n, t, i_a, i_z, shocks) = 10 non-j
+        n_nc2 = working_years - fam_shock_period  # = 36
+        V_nc2  = zeros(Float32, n_nc2, 2, 2, 5, 3, apnts_nc, zpnts, 2, 2, 2, 2)
+        PF_nc2 = copy(V_nc2)
 
-    # --- Post-family-shock working Vnc2 ---
-    println("Solving No College Post-Family-Shock Working...")
-    # Dims: (j, R, m, n, t, i_a, i_z, shocks) = 10 non-j
-    n_nc2 = working_years - fam_shock_period  # = 36
-    V_nc2  = zeros(Float32, n_nc2, 2, 2, 5, 3, apnts_nc, zpnts, 2, 2, 2, 2)
-    PF_nc2 = copy(V_nc2)
+        Vj_nc2  = zeros(Float32, 2, 2, 5, 3, apnts_nc, zpnts, 2, 2, 2, 2)
+        PFj_nc2 = copy(Vj_nc2)
 
-    Vj_nc2  = zeros(Float32, 2, 2, 5, 3, apnts_nc, zpnts, 2, 2, 2, 2)
-    PFj_nc2 = copy(Vj_nc2)
+        W_ret_first = @view W_nc[1, :,:,:,:,:,:,:,:,:,:]
+        V_nc2[n_nc2, :,:,:,:,:,:,:,:,:,:], PF_nc2[n_nc2, :,:,:,:,:,:,:,:,:,:] = Vnc2j_solve(nothing, W_ret_first, Vj_nc2, PFj_nc2, model, working_years)
 
-    W_ret_first = @view W_nc[1, :,:,:,:,:,:,:,:,:,:]
-    V_nc2[n_nc2, :,:,:,:,:,:,:,:,:,:], PF_nc2[n_nc2, :,:,:,:,:,:,:,:,:,:] = Vnc2j_solve(nothing, W_ret_first, Vj_nc2, PFj_nc2, model, working_years)
+        for j in (working_years-1):-1:(fam_shock_period+1)
+            idx = j - fam_shock_period
+            V_jp1 = @view V_nc2[idx+1, :,:,:,:,:,:,:,:,:,:]
+            V_nc2[idx, :,:,:,:,:,:,:,:,:,:], PF_nc2[idx, :,:,:,:,:,:,:,:,:,:] = Vnc2j_solve(V_jp1, nothing, Vj_nc2, PFj_nc2, model, j)
+        end
 
-    for j in (working_years-1):-1:(fam_shock_period+1)
-        idx = j - fam_shock_period
-        V_jp1 = @view V_nc2[idx+1, :,:,:,:,:,:,:,:,:,:]
-        V_nc2[idx, :,:,:,:,:,:,:,:,:,:], PF_nc2[idx, :,:,:,:,:,:,:,:,:,:] = Vnc2j_solve(V_jp1, nothing, Vj_nc2, PFj_nc2, model, j)
-    end
+        # --- Pre-family-shock working Vnc1 ---
+        println("Solving No College Pre-Family-Shock Working...")
+        # Dims: (j, R, t, i_a, i_z, shocks) = 8 non-j
+        n_nc1 = fam_shock_period  # = 7
+        Vj_nc1  = zeros(Float32, 2, 3, apnts_nc, zpnts, 2, 2, 2, 2)
+        PFj_nc1 = copy(Vj_nc1)
 
-    # --- Pre-family-shock working Vnc1 ---
-    println("Solving No College Pre-Family-Shock Working...")
-    # Dims: (j, R, t, i_a, i_z, shocks) = 8 non-j
-    n_nc1 = fam_shock_period  # = 7
-    Vj_nc1  = zeros(Float32, 2, 3, apnts_nc, zpnts, 2, 2, 2, 2)
-    PFj_nc1 = copy(Vj_nc1)
+        V_nc1  = zeros(Float32, n_nc1, 2, 3, apnts_nc, zpnts, 2, 2, 2, 2)
+        PF_nc1 = copy(V_nc1)
 
-    V_nc1  = zeros(Float32, n_nc1, 2, 3, apnts_nc, zpnts, 2, 2, 2, 2)
-    PF_nc1 = copy(V_nc1)
+        Vnc2_first = @view V_nc2[1, :,:,:,:,:,:,:,:,:,:]
+        V_nc1[n_nc1, :,:,:,:,:,:,:,:], PF_nc1[n_nc1, :,:,:,:,:,:,:,:] = Vnc1j_solve(nothing, Vnc2_first, Vj_nc1, PFj_nc1, model, fam_shock_period)
 
-    Vnc2_first = @view V_nc2[1, :,:,:,:,:,:,:,:,:,:]
-    V_nc1[n_nc1, :,:,:,:,:,:,:,:], PF_nc1[n_nc1, :,:,:,:,:,:,:,:] = Vnc1j_solve(nothing, Vnc2_first, Vj_nc1, PFj_nc1, model, fam_shock_period)
-
-    for j in (fam_shock_period-1):-1:1
-        V_jp1 = @view V_nc1[j+1, :,:,:,:,:,:,:,:]
-        V_nc1[j, :,:,:,:,:,:,:,:], PF_nc1[j, :,:,:,:,:,:,:,:] = Vnc1j_solve(V_jp1, nothing, Vj_nc1, PFj_nc1, model, j)
+        for j in (fam_shock_period-1):-1:1
+            V_jp1 = @view V_nc1[j+1, :,:,:,:,:,:,:,:]
+            V_nc1[j, :,:,:,:,:,:,:,:], PF_nc1[j, :,:,:,:,:,:,:,:] = Vnc1j_solve(V_jp1, nothing, Vj_nc1, PFj_nc1, model, j)
+        end
+        
+        open(joinpath(savepath, "NoSchool.jls"), "w") do io
+                serialize(io, (Vj_nc1 = Vj_nc1, PFj_nc1 = PFj_nc1,
+            Vj_nc2 = Vj_nc2, PFj_nc2 = PFj_nc2,
+            Wj_nc  = Wj_nc,  WPFj_nc = WPFj_nc,))
+            end
     end
 
     # -----------------------------------------------------------------------
@@ -106,7 +114,7 @@ function solve_model(model, savepath)
     println("Solving College Pre-Family-Shock Working...")
     # Dims: (j, R, t, degree, i_a, i_z, shocks) = 9 non-j
     n_c1 = fam_shock_period - 2  # = 7
-    Vj_c1  = zeros(Float32, n_c1, 2, 3, 2, apnts_c, zpnts, 2, 2, 2, 2)
+    Vj_c1  = zeros(Float32, 2, 3, 2, apnts_c, zpnts, 2, 2, 2, 2)
     PFj_c1 = copy(Vj_c1)
 
     V_c1  = zeros(Float32, n_c1, 2, 3, 2, apnts_c, zpnts, 2, 2, 2, 2)
@@ -149,12 +157,6 @@ function solve_model(model, savepath)
     PFs_1 = copy(Vs_1)
 
     Vs_1, PFs_1 = VSj_first_period(@view(Vs[1, :,:,:,:]), Vs_1, PFs_1, model)
-
-    open(joinpath(savepath, "NoSchool.jls"), "w") do io
-            serialize(io, (Vj_nc1 = Vj_nc1, PFj_nc1 = PFj_nc1,
-        Vj_nc2 = Vj_nc2, PFj_nc2 = PFj_nc2,
-        Wj_nc  = Wj_nc,  WPFj_nc = WPFj_nc,))
-        end
 
     open(joinpath(savepath, "School.jls"), "w") do io
             serialize(io, (Vj_c1  = Vj_c1,  PFj_c1  = PFj_c1,
